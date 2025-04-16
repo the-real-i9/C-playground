d36b7c8d5c3eaa43ccfdfe7f22151ebbc002206a
i9 <oluwarinolasam@gmail.com>
2025-04-16 15:43:04 +0100

    update

---
+++ gostrings/gostrings.h
@@ -0,0 +1,5 @@
+/* This C header file contains exercise implementations of Go's 'strings' package */
+
+void clone(char* dest, char *str) {
+  while(*dest++ = *str++);
+}
--- gostrings/main.c
+++ gostrings/main.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "gostrings.h"
+
 int main() {
+
+  char str[] = "Hello World!";
+
+  char theClone[sizeof str];
+
+  clone(theClone, str);
+
+  puts(theClone);
+
   return 0;
 }
