5ea5a90fb9448d106e26cf1ed5a745fb541faeb2
i9 <oluwarinolasam@gmail.com>
2025-04-23 20:09:18 +0100

    update

---
+++ singlyLinkedList/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct node {
+  int value;
+  struct node *next;
+};
+
+struct list {
+  struct node *head;
+  struct node *tail;
+};
+
+int main() {
+
+  struct list myList;
+
+  
+
+  return 0;
+}
